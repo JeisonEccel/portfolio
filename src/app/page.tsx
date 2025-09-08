08171afa6d4685f8751fbe4a59b7a2d00fd2c8f0
Jeison Eccel <projetojeison@hotmail.com>
2025-09-08 14:40:54 -0700

    Refs #7: Created About Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Introduction from "./(sections)/Introduction"
 import Skills from "./(sections)/Skill"
 import Projects from "./(sections)/Projects"
 import Experiences from "./(sections)/Experiences"
+import About from "./(sections)/About"
 
 export default function Page() {
   return (
@@ -14,6 +15,8 @@ export default function Page() {
       <Projects />
       <SectionDivider />
       <Experiences />
+      <SectionDivider />
+      <About />
     </>
   )
 }
