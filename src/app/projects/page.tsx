e1bf3fd7a2802340858a36a80a7cb88592f401a4
Jeison Eccel <projetojeison@hotmail.com>
2026-09-10 15:51:35 -0700

    Refs #16: Removed Guitar Practice V1 project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -11,17 +11,11 @@ import { Code, FolderGit, PencilRuler } from "lucide-react"
 import { personalWebsite } from "@/constants/projects/personal-website"
 import { Links } from "@/components/links"
 import { myFinancesBackend } from "@/constants/projects/my-finances-backend"
-import { guitarPracticeV1 } from "@/constants/projects/guitar-practice-v1"
-import { guitarPracticeV2 } from "@/constants/projects/guitar-practice-v2"
+import { praticaTool } from "@/constants/projects/pratica-tool"
 import { cn } from "@/lib/utils"
 import { Project } from "@/types/projects"
 
-const projectsList = [
-  guitarPracticeV2,
-  myFinancesBackend,
-  personalWebsite,
-  guitarPracticeV1,
-]
+const projectsList = [praticaTool, myFinancesBackend, personalWebsite]
 
 function ProjectCard(project: { invert: boolean } & Project) {
   const [expanded, setExpanded] = useState(false)
